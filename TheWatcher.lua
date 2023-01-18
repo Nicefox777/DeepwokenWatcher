0efe423236a6bb8d2cc3a616d0c3c1446a8a2b8c
123465525 <a0923699883@gmail.com>
2023-01-19 00:24:32 +0800

    Update TheWatcher.lua

--- TheWatcher.lua
+++ TheWatcher.lua
@@ -7,6 +7,17 @@ if game.PlaceId == 6032399813 or game.PlaceId == 6473861193 or game.PlaceId == 5
         local charcreator = game:GetService("Workspace").CharacterCreator
         local TimeNow = DateTime.now()
 
+        local serverregion = game:GetService("ReplicatedStorage")["SERVER_REGION"].Value
+        local serverrealm
+
+        if game.PlaceId == 6032399813 then
+            serverrealm = "Etrean Luminant"
+        elseif game.PlaceId == 6473861193 then
+            serverrealm = "East Luminant"
+        elseif game.PlaceId == 5735553160 then
+            serverrealm = "The Depth"
+        end
+
         print(#players)
 
         --Player Name
@@ -456,7 +467,7 @@ if game.PlaceId == 6032399813 or game.PlaceId == 6473861193 or game.PlaceId == 5
            {
                ["title"] = "**[Player Detected] Here is the INFO**",
                ["description"] = 
-               "Executor: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**" .. "\n" .. "\n" .. "Current Server Name: " .. "**"..game:GetService("ReplicatedStorage")["SERVER_NAME"].Value .. "**" .. "\n" .. "\n" .. "Time: " .. "**" .. TimeNow:FormatLocalTime("LLLL", "zh-tw") .. "**" .. "\n" .. "\n" .. "Players in the Server: " .. "(" .. #players .. "/" .. "20" .. ")" .. "\n" .. "\n" .. infoo["player1"].."\n"..infoo["player2"].."\n"..infoo["player3"].."\n"..infoo["player4"].."\n"..infoo["player5"].."\n"..infoo["player6"].."\n"..infoo["player7"].."\n"..infoo["player8"].."\n"..infoo["player9"].."\n"..infoo["player10"].."\n"..infoo["player11"].."\n"..infoo["player12"].."\n"..infoo["player13"].."\n"..infoo["player14"].."\n"..infoo["player15"].."\n"..infoo["player16"].."\n"..infoo["player17"].."\n"..infoo["player18"].."\n"..infoo["player19"].."\n"..infoo["player20"],
+               "Executor: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**" .. "\n" .. "\n" .. "Current Server Realm: " .. "**" .. serverrealm .. "**" .. "\n" .. "\n" .. "Current Server Name: " .. "**" .. game:GetService("ReplicatedStorage")["SERVER_NAME"].Value .. "**" .. "\n" .. "\n" .. "Time: " .. "**" .. TimeNow:FormatLocalTime("LLLL", "zh-tw") .. "**" .. "\n" .. "\n" .. "Players in the Server: " .. "(" .. #players .. "/" .. "20" .. ")" .. "\n" .. "\n" .. infoo["player1"].."\n"..infoo["player2"].."\n"..infoo["player3"].."\n"..infoo["player4"].."\n"..infoo["player5"].."\n"..infoo["player6"].."\n"..infoo["player7"].."\n"..infoo["player8"].."\n"..infoo["player9"].."\n"..infoo["player10"].."\n"..infoo["player11"].."\n"..infoo["player12"].."\n"..infoo["player13"].."\n"..infoo["player14"].."\n"..infoo["player15"].."\n"..infoo["player16"].."\n"..infoo["player17"].."\n"..infoo["player18"].."\n"..infoo["player19"].."\n"..infoo["player20"],
                ["type"] = "rich",
                ["color"] = tonumber(0x48f745),
                ["image"] = { 
